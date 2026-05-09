aec43730eb07a17d9e66413555ed0821b6bdc128
unknown <c0dedev.zeroone@gmail.com>
2026-05-09 13:14:23 +0800

    fix server merge conflict

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ let cart = [];
 /* =========================
    🌐 BACKEND URL (RENDER)
 ========================= */
-const BASE_URL = "https://food-shop-q4vc.onrender.com";
+const BASE_URL = "https://food-shop-j5pa.onrender.com";
 
 const API = `${BASE_URL}/api/products`;
 const API_IMG = BASE_URL;
